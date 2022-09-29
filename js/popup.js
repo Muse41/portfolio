592c797277492eb502f6620fcc507fb662192815
muse <jisik24@naver.com>
2022-09-29 11:59:38 +0900

    포폴1

---
+++ js/popup.js
@@ -0,0 +1,65 @@
+   $(function(){ 
+
+      $('#pop_up1').click(function(){
+        $('#pop_up1').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.pop1').click(function(){
+        $('#pop_up1').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+      
+      $('#pop_up2').click(function(){
+        $('#pop_up2').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.pop2').click(function(){
+        $('#pop_up2').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+
+      $('#pop_up3').click(function(){
+        $('#pop_up3').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.pop3').click(function(){
+        $('#pop_up3').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+
+      $('#pop_up4').click(function(){
+        $('#pop_up4').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.pop4').click(function(){
+        $('#pop_up4').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+
+      $('#pop_up5').click(function(){
+        $('#pop_up5').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.pop5').click(function(){
+        $('#pop_up5').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+
+      $('#pop_up6').click(function(){
+        $('#pop_up6').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.sangse_box .bed').click(function(){
+        $('#pop_up6').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+      $('#pop_up7').click(function(){
+        $('#pop_up7').fadeOut(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+      })
+      $('.sangse_box .sofa').click(function(){
+        $('#pop_up7').fadeIn(300);
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      })
+
+    })
